1c0b3b2099df6fdf94cebb5575f6ae4a2efc2b05
PANKAJ KUMAR <110244454+iampankaj14@users.noreply.github.com>
2023-10-01 22:47:26 +0530

    Add files via upload

---
+++ solar_planets.py
@@ -0,0 +1,70 @@
+import cv2
+
+img = cv2.imread("sloar-system.py")
+cv2.imshow("output",img)
+cv2.waitKey(0)
+
+cv2.putText(img,
+           "Sun",
+           (20,300),
+           cv2.FONT_HERSHEY_COMPLEX,
+           0.5,
+           (255,255,255)
+           )
+
+cv2.putText(img,
+           "Mercury",
+           (20,300),
+           cv2.FONT_HERSHEY_COMPLEX,
+           0.5,
+           (255,255,255)
+           )
+cv2.putText(img,
+           "Venus",
+           (20,300),
+           cv2.FONT_HERSHEY_COMPLEX,
+           0.5,
+           (255,255,255)
+           )
+cv2.putText(img,
+           "Earth",
+           (20,300),
+           cv2.FONT_HERSHEY_COMPLEX,
+           0.5,
+           (255,255,255)
+           )
+cv2.putText(img,
+           "Mars",
+           (20,300),
+           cv2.FONT_HERSHEY_COMPLEX,
+           0.5,
+           (255,255,255)
+           )
+cv2.putText(img,
+           "Jupiter",
+           (20,300),
+           cv2.FONT_HERSHEY_COMPLEX,
+           0.5,
+           (255,255,255)
+           )
+cv2.putText(img,
+           "Saturn",
+           (20,300),
+           cv2.FONT_HERSHEY_COMPLEX,
+           0.5,
+           (255,255,255)
+           )
+cv2.putText(img,
+           "Uranus",
+           (20,300),
+           cv2.FONT_HERSHEY_COMPLEX,
+           0.5,
+           (255,255,255)
+           )
+cv2.putText(img,
+           "Neptune",
+           (20,300),
+           cv2.FONT_HERSHEY_COMPLEX,
+           0.5,
+           (255,255,255)
+           )
